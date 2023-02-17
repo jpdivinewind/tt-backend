cd845d2a9ec186303f19fb14c01b7adb33b77bee
Illia Chernysh <jpdivinewind@gmail.com>
2023-02-18 00:07:12 +0200

    Added CORS configs

--- src/http/index.ts
+++ src/http/index.ts
@@ -2,12 +2,14 @@ import Fastify from 'fastify';
 import {PORT} from '../config';
 import {log} from '../logger';
 import routes from './routes';
+import cors from '@fastify/cors';
 
 const app = Fastify({logger: log});
 
 routes(app);
 
 async function listen() {
+    await app.register(cors, {origin: '*'});
     await app.listen({host: '0.0.0.0', port: PORT});
 }
 
--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,10 +1,11 @@
 import {Server as SocketServer} from 'socket.io';
 import {Server} from 'http';
+import {CORS_ORIGIN} from '../config';
 
 let socketServer: SocketServer | null = null;
 
 function connect(server: Server) {
-    socketServer = new SocketServer(server);
+    socketServer = new SocketServer(server, {cors: {origin: CORS_ORIGIN}});
 }
 
 function emit(ev: string, ...args: unknown[]) {
