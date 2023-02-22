f65095d319df983879e8458b4574d05980162bd6
Illia Chernysh <jpdivinewind@gmail.com>
2023-02-22 14:34:01 +0200

    Use CORS_ORIGIN env variable

--- src/http/index.ts
+++ src/http/index.ts
@@ -3,13 +3,14 @@ import {PORT} from '../config';
 import {log} from '../logger';
 import routes from './routes';
 import cors from '@fastify/cors';
+import {CORS_ORIGIN} from '../config';
 
 const app = Fastify({logger: log});
 
 routes(app);
 
 async function listen() {
-    await app.register(cors, {origin: '*'});
+    await app.register(cors, {origin: CORS_ORIGIN});
     await app.listen({host: '0.0.0.0', port: PORT});
 }
 
