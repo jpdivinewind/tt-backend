6fee2faf5c171a4348ed9b9845c1d4f6ee383830
Illia Chernysh <jpdivinewind@gmail.com>
2023-02-22 15:41:10 +0200

    Expanded CORS settings

--- README.md
+++ README.md
@@ -8,6 +8,9 @@ LOG_LEVEL -- default: debug
 MONGO_URL -- default: mongodb://127.0.0.1:27017/tt-backend
 PORT -- default: 3000
 CORS_ORIGIN -- cors origin
+CORS_METHODS -- default: GET,POST
+CORS_HEADERS -- default: Content-Type,Authorization
+CORS_CREDS -- default: true
 JOB_UPDATE_EXCHANGE_RATES_RULE -- in cron format, default: * /2 * * * *
 COINMARKETCAP_API_KEY -- coinmarketcap.com api key
 CRYPTOCOMPARE_API_KEY -- cryptocompare.com api key
--- src/http/index.ts
+++ src/http/index.ts
@@ -1,5 +1,5 @@
 import Fastify from 'fastify';
-import {PORT} from '../config';
+import {CORS_CREDS, CORS_HEADERS, CORS_METHODS, PORT} from '../config';
 import {log} from '../logger';
 import routes from './routes';
 import cors from '@fastify/cors';
@@ -10,7 +10,12 @@ const app = Fastify({logger: log});
 routes(app);
 
 async function listen() {
-    await app.register(cors, {origin: CORS_ORIGIN});
+    await app.register(cors, {
+        origin: CORS_ORIGIN,
+        methods: CORS_METHODS,
+        allowedHeaders: CORS_HEADERS,
+        credentials: CORS_CREDS,
+    });
     await app.listen({host: '0.0.0.0', port: PORT});
 }
 
